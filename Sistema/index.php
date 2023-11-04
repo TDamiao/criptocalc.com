4de9e9e62d02ad612ddfc4a7c1545929ec9d6a3d
TDamiao <thiagodamiaosoares@gmail.com>
2023-11-04 19:59:35 -0300

    add ajax e jquerry

--- Sistema/index.php
+++ Sistema/index.php
@@ -9,35 +9,47 @@
     <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&family=Open+Sans&display=swap" rel="stylesheet">
+<script
+  src="https://code.jquery.com/jquery-3.7.1.slim.min.js"
+  integrity="sha256-kmHvs0B+OpCW5GVHUNjv9rOmY0IvSIRcf7zGUDTDQM8="
+  crossorigin="anonymous"></script>
 </head>
 <body>
     <section class="main-content">
   <center><img class="logo" src="./assets/img/logo.svg" alt="logo"></center>
 
-
+    <form  method="post">
         <div class="moedasleft"> 
         <p>selecione a moeda 1</p>
             <select class="cbmoedasleft"  name="moedaA">
                 <option>BTC</option>
                 <option>USD</option>
                 <option>BRL</option>
-                <option>ERU</option>
+                <option>EUR</option>
             </select>
-            <input type="text"/>
+            <input type="text"  id="moeda2" value="1">
 
         </div>
 
         <div class="moedasright"> 
         <p>selecione a moeda 2</p>
-            <input type="text"  placeholder="<?php include('cotacao.php') ?> " >  
-            <select class="cbmoedasright" name="moedaB">
+            <input type="text" value="<?php include('cotacao.php') ?> " >  
+            <select class="cbmoedasright" name="moedaB" >
                 <option>USD</option>
                 <option>BTC</option>
                 <option>BRL</option>
-                <option>ERU</option>
+                <option>EUR</option>
             </select>
         </div>
+        <input type="submit">
+        </form>
 
+        <script>
+    $("$moeda2").change(function(){
+        var id_categoria = $(this).val();
+        $("#retorno").load("cotacao.php");
+    });
+</script>
 
     </section>
 </body>
